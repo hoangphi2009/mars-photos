c90dcb9a970dee77a20a0802a7394aa39b403ac3
Hoang Phi <phido76792@gmail.com>
2023-12-04 18:18:06 +0700

    code and fix

---
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.marsphotos.ui.screens
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.network.MarsApi
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+
+sealed interface MarsUiState {
+    data class Success(val photos: String) : MarsUiState
+    object Error : MarsUiState
+    object Loading : MarsUiState
+}
+
+class MarsViewModel : ViewModel() {
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
+        private set
+
+    init {
+        getMarsPhotos()
+    }
+
+
+    fun getMarsPhotos() {
+        viewModelScope.launch {
+            marsUiState = MarsUiState.Loading
+            marsUiState = try {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                MarsUiState.Success(
+                    "Success: ${listResult.size} Mars photos retrieved"
+                )
+            } catch (e: IOException) {
+                MarsUiState.Error
+            } catch (e: HttpException) {
+                MarsUiState.Error
+            }
+        }
+    }
+}
